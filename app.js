2f4937f5f906f8cec2db88ba5d7d5843a343f678
MichaelJoson <mjoson@mail.bradley.edu>
2022-07-16 13:25:46 -0500

    Redirect to spotify authorization page

---
+++ app.js
@@ -0,0 +1,30 @@
+var redirect_uri = "http://127.0.0.1:5500/index.html" //redirect to local server
+
+var client_id = "";
+var client_secret = "";
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+
+function onPageLoad()
+{
+
+}
+
+
+
+function requestAuthorization(){
+    //get client id and client secret from user input
+    client_id = document.getElementById("clientId").value;
+    client_secret = document.getElementById("clientSecret").value;
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret); 
+
+    //redirect to Spotify's authorization page
+    let url = AUTHORIZE;
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url;
+}
