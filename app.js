75a4c32c0c42e345a4da57361bbe102165e440cb
MichaelJoson <mjoson@mail.bradley.edu>
2022-07-17 19:08:39 -0500

    Retrieve access and refresh token

--- app.js
+++ app.js
@@ -1,16 +1,88 @@
+//declare variables
 var redirect_uri = "http://127.0.0.1:5500/index.html" //redirect to local server
 
 var client_id = "";
 var client_secret = "";
+var access_token = null;
+var refresh_token = null;
+
 
 const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token";
 
+//handles logging into spotify page
 function onPageLoad()
 {
+    client_id = localStorage.getItem("client_id");
+    client_secret = localStorage.getItem("client_secret");
 
+    if ( window.location.search.length > 0 ){
+        handleRedirect();
+    }
 }
 
+//Handles redirect from spotify login page back to web app
+function handleRedirect()
+{
+    let code = getCode();
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri);
+}
 
+//fetch access token from code
+function fetchAccessToken(code)
+{
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+//authorize login
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
+
+//receive response for authorization api
+function handleAuthorizationResponse(){
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if ( data.refresh_token  != undefined ){
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+//receive code from spotify login
+function getCode()
+{
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+}
 
 function requestAuthorization(){
     //get client id and client secret from user input
